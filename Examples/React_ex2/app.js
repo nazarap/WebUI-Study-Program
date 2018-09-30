137a0ef528096a819980164e0411e822837aeac8
Nazar Oryschuk <nazar125125@gmail.com>
2018-09-30 12:46:36 +0300

    Created example for react config

---
+++ Examples/React_ex2/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <aside>
+    <h1>Hello, world!</h1>
+    <h4>The page is rendered by React</h4>
+  </aside>,
+  document.getElementById('root')
+);
---
+++ Examples/React_ex2/webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
+};
